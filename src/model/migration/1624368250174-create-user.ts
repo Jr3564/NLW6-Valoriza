80abdd1d8c8bfa08aefb30950e9ae3e3da4c74f8
jr3564 <rodolfojr94@gmail.com>
2021-06-22 16:08:36 -0300

    created migration users

--- src/model/migration/1624368250174-create-user.ts
+++ src/model/migration/1624368250174-create-user.ts
@@ -1,40 +1,45 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
+/* eslint-disable import/prefer-default-export */
+/* eslint-disable class-methods-use-this */
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class createUser1624368250174 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.createTable({
-      name: 'users',
-      columns: [
-        {
-          name: 'id',
-          type: 'uuid',
-          isPrimary: true,
-        },
-        {
-          name: 'name',
-          type: 'varchar',
-        },
-        {
-          name: 'email',
-          type: 'varchar',
-        },
-        {
-          name: 'admin',
-          type: 'boolean',
-          default: false,
-        },
-        {
-          name: 'created_at',
-          type: 'timestamp',
-          default: 'now()',
-        },
-        {
-          name: 'updated_at',
-          type: 'timestamp',
-          default: 'now()',
-        },
-      ],
-    });
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+          },
+          {
+            name: 'admin',
+            type: 'boolean',
+            default: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
