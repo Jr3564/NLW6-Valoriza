01e45474e79f4ef0fc82440af3c0a9c9ad347dd7
jr3564 <rodolfojr94@gmail.com>
2021-06-23 20:46:58 -0300

    Emcripted password

--- src/service/UserService.ts
+++ src/service/UserService.ts
@@ -1,12 +1,18 @@
 import IUserRequest from '../interfaces/UserInterface';
 import { UserModel } from '../model/UserModel';
+import { hash } from 'bcryptjs';
 
 export class UserService {
   async create(user : IUserRequest) {
     const userModel = new UserModel();
+    
     if (!user.email) throw new Error('Email incorrect');
+
     const userAlreadyExists = await userModel.getByEmail(user.email);
     if (userAlreadyExists) throw new Error('User already exists');
-    return userModel.create(user);
+
+    const passwordHash = await hash(user.password, 8);
+
+    return userModel.create({ ...user, password: passwordHash });
   }
 }
