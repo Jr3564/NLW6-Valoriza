fef19a6f68b3f1cf3cf4ac1bb966700664f5b00b
jr3564 <rodolfojr94@gmail.com>
2021-06-22 23:34:42 -0300

    middleware ensureAdmin created

---
+++ src/middlewares/ensureAdmin.ts
@@ -0,0 +1,12 @@
+import { Response, Request, NextFunction } from 'express';
+
+const ensureAdmin = (_req: Request, res: Response, next: NextFunction) => {
+
+  const admin = true;
+
+  if (admin) return next();
+
+  return res.status(401).json({ error: 'Unauthorized'})
+}
+
+export { ensureAdmin };
--- src/routes.ts
+++ src/routes.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 
 import { TagController } from './controller/TagController';
 import { UserController } from './controller/UserController';
-
+import { ensureAdmin } from './middlewares/ensureAdmin'
 const router = Router();
 
 const userController = new UserController();
@@ -12,7 +12,7 @@ router.route('/users')
   .post(userController.create);
 
 router.route('/tags')
-  .post(tagController.create);
+  .post(ensureAdmin, tagController.create);
 
 
 export { router };
