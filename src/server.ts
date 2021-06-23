421bb1ad05233f50fc7ac503026306fb16915f54
jr3564 <rodolfojr94@gmail.com>
2021-06-22 22:31:40 -0300

    Created error middleware

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,29 @@
+import { NextFunction, Request, Response } from "express";
+
 require('reflect-metadata');
 
 require('typeorm').createConnection();
 
 const express = require('express');
+
+require('express-async-errors');
+
 const { router } = require('./routes');
 
 const app = express();
 
 app.use(express.json());
 app.use(router);
+
+app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
+  if (err instanceof Error) {
+    return res.status(400).json({ error: err.message })
+  }
+  return res.status(500).json({
+    status: "error",
+    message: "Internal Server Error"
+  })
+})
 
 const PORT = 3000;
 
