0da5fdac03cc9d31a6b84c88b081325e1667d6d2
jr3564 <rodolfojr94@gmail.com>
2021-06-24 13:51:37 -0300

    Created compliment route

--- src/routes.ts
+++ src/routes.ts
@@ -3,11 +3,13 @@ import { Router } from 'express';
 import { TagController } from './controller/TagController';
 import { UserController } from './controller/UserController';
 import { AuthUserController } from './controller/AuthUserController';
+import { ComplimentController } from './controller/ComplimentController';
 import { ensureAdmin } from './middlewares/ensureAdmin'
 
 const userController = new UserController();
 const tagController = new TagController();
 const authController = new AuthUserController();
+const complimentController = new ComplimentController();
 
 const router = Router();
 
@@ -19,6 +21,9 @@ router.route('/users')
 
 router.route('/tags')
   .post(ensureAdmin, tagController.create);
+
+router.route('/compliments')
+  .post(ensureAdmin, complimentController.create);
 
 
 export { router };
