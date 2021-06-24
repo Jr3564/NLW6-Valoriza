791fd5d0b345b4d350475f6942902825020506cf
jr3564 <rodolfojr94@gmail.com>
2021-06-24 12:55:13 -0300

    Created compimentsInterface

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,2 +1,3 @@
 export { default as UserAuthInterface } from './UserAuthInterface';
 export { default as UserInterface } from './UserInterface';
+export { default as ComplimentInterface } from './ComplimentInterface';
