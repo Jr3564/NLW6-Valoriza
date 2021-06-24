f1f28d5ef8c05a5554743f0e109988abcc9024f9
jr3564 <rodolfojr94@gmail.com>
2021-06-24 13:51:09 -0300

    Created compliment controller

---
+++ src/controller/ComplimentController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express';
+import { ComplimentService } from '../service/ComplimentService';
+
+export class ComplimentController {
+  async create(request: Request, response: Response) {
+    const { tag_id, user_sender, user_receiver, message } = request.body;
+
+    const complimentService = new ComplimentService();
+
+    const compliment = await complimentService.create({ tag_id, user_sender, user_receiver, message });
+
+    return response.json(compliment);
+  }
+}
