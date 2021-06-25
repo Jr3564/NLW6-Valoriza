396cc03a83158a309df8ee20794a5ae73c173442
jr3564 <rodolfojr94@gmail.com>
2021-06-25 07:44:42 -0300

    Redirected ensureAuthenticate

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,2 +1,3 @@
 export { default as ensureAdmin } from './ensureAdmin';
 export { default as errorHandler } from './errorHandler';
+export { default as ensureAuthenticated } from './ensureAuthenticated';
