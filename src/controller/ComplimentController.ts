774e5f601a8bcf3da57381351714b6d050942c44
jr3564 <rodolfojr94@gmail.com>
2021-06-25 09:12:29 -0300

    Get user_sender by id

--- src/controller/ComplimentController.ts
+++ src/controller/ComplimentController.ts
@@ -3,7 +3,8 @@ import { ComplimentService } from '../service';
 
 export default class ComplimentController {
   async create(request: Request, response: Response) {
-    const { tag_id, user_sender, user_receiver, message } = request.body;
+    const { tag_id, user_receiver, message } = request.body;
+    const user_sender = request.user_id;
 
     const complimentService = new ComplimentService();
 
